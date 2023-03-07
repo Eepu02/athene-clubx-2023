181c263e9e875afac66d1d5c33a34a2c87fdcc89
Eepu02 <eemil.lahteenmaki@gmail.com>
2023-03-07 17:55:59 +0200

    Renaming

--- photo.js
+++ photo.js
@@ -1,90 +1,100 @@
 // for easy future referencing
 const canvas = document.querySelector(".product-slide");
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 const frameCount = 108;
 
-// const loader = document.querySelector(".loader-bg");
-
-// Stores the images for fast access
-const images = []
+// Stores the images in an array for fast access
+const images = [];
 const imgPath = () => {
-    // if(screen.availWidth > 800) {
-    //     return "short_desktop_img_sequence";
-    // } else {
-    //     return "short_mobile_img_sequence";
-    // }
-    return screen.availWidth > 800 ? "short_desktop_img_sequence" : "short_mobile_img_sequence"
+	return screen.availWidth > 800
+		? "short_desktop_img_sequence"
+		: "short_mobile_img_sequence";
 };
 
-// addEventListener("resize", (event) => {
-//     path
-// });
-
-// takes index of img as parameter, returns src of img
-const currentFrame = index => (
-    // `assets/1080_50/${index.toString().padStart(4, '0')}.jpg`
-    `assets/${imgPath()}/${index.toString().padStart(4, '0')}.webp`
-)
+// Takes index of img as parameter, returns src of img
+const currentFrame = (index) =>
+	`assets/${imgPath()}/${index.toString().padStart(4, "0")}.webp`;
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+const wrapper = document.querySelector(".canvas-wrapper");
+const canvasPos = wrapper.getBoundingClientRect().top;
+
+const renderImg = (index = 0) => {
+	requestAnimationFrame(() => updateImage(index));
+};
+
+preloadImages();
 
 let img = new Image();
+
+// Responsible for initial load
+img.onload = () => {
+	renderImg();
+};
 
 // Set source to 1st frame
 img.src = currentFrame(0);
 
+let scale, centerShift_x, centerShift_y;
+
 // Calculate scale and img draw position
-const scale = Math.max(canvas.width / img.width, canvas.height / img.height);
-const centerShift_x = (canvas.width - img.width * scale) / 2;
-const centerShift_y = (canvas.height - img.height * scale) / 2;
-
-// Load first image on page load
-img.onload = function() {
-  context.drawImage(img, 0,0, img.width, img.height, centerShift_x, centerShift_y, img.width*scale, img.height*scale);
-}
-
-// Updates img path and draws updated image
-const updateImage = index => {
-    let photo = images[index];
-    // console.log(photo.src)
-    // img.src = currentFrame(index);
-    context.drawImage(images[index], 0,0, photo.width, photo.height, centerShift_x, centerShift_y, photo.width*scale, photo.height*scale);
-}
-
-const wrapper = document.querySelector(".canvas-wrapper");
-const canvasPos = wrapper.getBoundingClientRect().top;
-window.addEventListener('scroll', () => {
-  
-    const sizes = wrapper.getBoundingClientRect();
-    // if(window.pageYOffset > canvasPos + sizes.height) {
-    // Fires when scroll is at its end
-    //   wrapper.style.position = 'relative';
-    //   wrapper.style.marginTop = sizes.height + 'px';
-    //   console.log(window.pageYOffset)
-    // } else {
-    //   wrapper.style.position = 'sticky';
-    //   wrapper.style.marginTop = '0px';
-    // }
-
-    const scrollFraction = Math.max(0, (window.pageYOffset - canvasPos) / sizes.height);
-    
-    const frameIndex = Math.min(
-      frameCount - 1,
-      Math.floor(scrollFraction * frameCount)
-    );
-    requestAnimationFrame(() => updateImage(frameIndex));
-  });
-
-// Loads images into memory for faster access
-async function preloadImages() {
-    for(let i = 1; i < frameCount; i++) {
-        const img = new Image();
-        img.src = currentFrame(i);
-        images[i] = img;
-    }
-    // requestAnimationFrame(() => updateImage(0));
-    console.log("All images loaded!");
+const updateScaling = () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+	centerShift_x = (canvas.width - img.width * scale) / 2;
+	centerShift_y = (canvas.height - img.height * scale) / 2;
 };
 
-preloadImages();
+updateScaling();
+
+// Updates img path and draws updated image
+const updateImage = (index = 0) => {
+	const photo = images[index];
+	context.drawImage(
+		photo,
+		0,
+		0,
+		photo.width,
+		photo.height,
+		centerShift_x,
+		centerShift_y,
+		photo.width * scale,
+		photo.height * scale
+	);
+};
+
+const getFrameIndex = () => {
+	// Get size and position of canvas wrapper
+	const sizes = wrapper.getBoundingClientRect();
+	const scrollFraction = Math.max(
+		0,
+		(window.pageYOffset - canvasPos) / sizes.height
+	);
+
+	// Calculate frame index based on scroll position
+	const frameIndex = Math.min(
+		frameCount - 1,
+		Math.floor(scrollFraction * frameCount)
+	);
+	return frameIndex;
+};
+
+addEventListener("scroll", () => {
+	renderImg(getFrameIndex());
+});
+
+addEventListener("resize", () => {
+	updateScaling();
+	renderImg(getFrameIndex());
+});
+
+// Loads images into memory for faster access
+const preloadImages = () => {
+	for (let i = 0; i < frameCount; i++) {
+		const img = new Image();
+		img.src = currentFrame(i);
+		images[i] = img;
+	}
+};
