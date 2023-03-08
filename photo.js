4084452757fc7095b047eb343419fcb4b18c0119
Eepu02 <eemil.lahteenmaki@gmail.com>
2023-03-08 12:48:06 +0200

    Works for real

--- photo.js
+++ photo.js
@@ -1,72 +1,28 @@
 // for easy future referencing
 const canvas = document.querySelector(".product-slide");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 const context = canvas.getContext("2d");
 const frameCount = 108;
 
-// Stores the images in an array for fast access
+// Stores the images in an array for quick  access
 const images = [];
 const imgPath = () => {
-	return screen.availWidth > 800
+	// At less than around 1100 pixels the desktop text overflows,
+	// so switch to mobile (portrait) images
+	return screen.availWidth > 1100
 		? "short_desktop_img_sequence"
 		: "short_mobile_img_sequence";
 };
 
 // Takes index of img as parameter, returns src of img
-const currentFrame = (index) =>
+const getImgPath = (index) =>
 	`assets/${imgPath()}/${index.toString().padStart(4, "0")}.webp`;
 
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
 const wrapper = document.querySelector(".canvas-wrapper");
 const canvasPos = wrapper.getBoundingClientRect().top;
 
-// Updates img path and draws updated image
-function updateImage(index = 0) {
-	const photo = images[index];
-	console.log(photo);
-	context.drawImage(
-		photo,
-		0,
-		0,
-		photo.width,
-		photo.height,
-		centerShift_x,
-		centerShift_y,
-		photo.width * scale,
-		photo.height * scale
-	);
-}
-
-function renderImg(index = 0) {
-	requestAnimationFrame(() => updateImage(index));
-}
-
-preloadImages();
-
-let img = new Image();
-
-// Responsible for initial load
-img.onload = () => {
-	console.log(img);
-	renderImg();
-};
-
-// Set source to 1st frame
-img.src = currentFrame(0);
-
-let scale, centerShift_x, centerShift_y;
-
-// Calculate scale and img draw position
-const updateScaling = () => {
-	canvas.height = window.innerHeight;
-	canvas.width = window.innerWidth;
-	scale = Math.max(canvas.width / img.width, canvas.height / img.height);
-	centerShift_x = (canvas.width - img.width * scale) / 2;
-	centerShift_y = (canvas.height - img.height * scale) / 2;
-};
-
-updateScaling();
-
+// Returns the frame index (integer) based on scroll position
 const getFrameIndex = () => {
 	// Get size and position of canvas wrapper
 	const sizes = wrapper.getBoundingClientRect();
@@ -83,6 +39,51 @@ const getFrameIndex = () => {
 	return frameIndex;
 };
 
+preloadImages();
+
+let img = new Image();
+
+// Responsible for initial load
+img.onload = () => {
+	updateScaling();
+	drawImage(img);
+};
+
+// Set source to 1st frame
+img.src = getImgPath(getFrameIndex());
+
+let scale, centerShift_x, centerShift_y;
+
+// Calculate scale and img draw position
+const updateScaling = () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+	scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+	centerShift_x = (canvas.width - img.width * scale) / 2;
+	centerShift_y = (canvas.height - img.height * scale) / 2;
+};
+
+// This is called separately at initial load so it's
+// a standalone function
+function drawImage(photo) {
+	context.drawImage(
+		photo,
+		0,
+		0,
+		photo.width,
+		photo.height,
+		centerShift_x,
+		centerShift_y,
+		photo.width * scale,
+		photo.height * scale
+	);
+}
+
+// Render image uses requestAnimationFrame which causes minimal flicker
+function renderImg(index = 0) {
+	requestAnimationFrame(() => drawImage(images[index]));
+}
+
 addEventListener("scroll", () => {
 	renderImg(getFrameIndex());
 });
@@ -96,7 +97,7 @@ addEventListener("resize", () => {
 function preloadImages() {
 	for (let i = 0; i < frameCount; i++) {
 		const img = new Image();
-		img.src = currentFrame(i);
+		img.src = getImgPath(i);
 		images[i] = img;
 	}
 }
